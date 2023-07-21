9cd9c64e7a2cac268c4321260b2abe885d8e3d66
Philipp Leutner <philipp.leutner@assambra.com>
2023-07-21 12:24:48 +0200

    Removed unused GreetingService

--- Free-Server/free-server-app-api/src/main/java/com/assambra/app/constant/Commands.java
+++ Free-Server/free-server-app-api/src/main/java/com/assambra/app/constant/Commands.java
@@ -2,8 +2,5 @@ package com.assambra.app.constant;
 
 public final class Commands {
 
-    public static final String HELLO = "hello";
-    public static final String GO = "go";
-
     private Commands() {}
 }
