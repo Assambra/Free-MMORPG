ef153e8a395737fe7214c05b7866b578d662d708
Philipp Leutner <philipp.leutner@assambra.com>
2023-07-23 13:10:10 +0200

    Close #8 A ScriptableObject that holds a string for ClientLog so that it is persistent if we change menu scenes

--- Free-Client/Assets/Free-Client/Scripts/UserInterface/UIClientLog.cs
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/UIClientLog.cs
@@ -5,35 +5,36 @@ using UnityEngine;
 public class UIClientLog : MonoBehaviour
 {
     [SerializeField] TMP_Text textFieldServerLog;
+    [SerializeField] ClientLogFile clientLogFile;
 
     private NetworkManager networkManager;
 
-    private string logFile;
 
     private void Awake()
     {
         networkManager = GameObject.FindObjectOfType<NetworkManager>();
         networkManager.UIClientLog = this;
+
     }
 
     void Update()
     {
-        textFieldServerLog.text = logFile;
+        textFieldServerLog.text = clientLogFile.File;
     }
 
     public void ServerLogMessageInfo(string message)
     {
-        logFile += "<color=white>Info: " + message + "</color><br>";
+        clientLogFile.File += "<color=white>Info: " + message + "</color><br>";
     }
 
     public void ServerLogMessageSuccess(string message)
     {
-        logFile += "<color=green>Success: " + message + "</color><br>";
+        clientLogFile.File += "<color=green>Success: " + message + "</color><br>";
     }
 
     public void ServerLogMessageError(string message)
     {
-        logFile += "<color=red>Error: " + message + "</color><br>";
+        clientLogFile.File += "<color=red>Error: " + message + "</color><br>";
     }
 
     private string CreateNewLine(string text)
