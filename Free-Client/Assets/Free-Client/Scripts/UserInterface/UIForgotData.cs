ae234d7a6bd4d82cc966135457cd45b6882f4f15
Philipp Leutner <philipp.leutner@assambra.com>
2023-07-31 18:46:12 +0200

    Reviced the unity ForgetPassword scene is now ForgotData there you can request a new password and or request your username with a valid e-mail address. Some helper classes to create a tab based window, new tab textures.

--- Free-Client/Assets/Free-Client/Scripts/UserInterface/UIForgotData.cs
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/UIForgotData.cs
@@ -6,8 +6,11 @@ public class UIForgotData : MonoBehaviour
 {
     public Button buttonBack;
     public Button buttonSendPassword;
+    public Button buttonSendUsername;
 
     [SerializeField] private TMP_InputField inputFieldUsernameOrEMail;
+    [SerializeField] private GameObject gameObjectForgotPassword;
+    [SerializeField] private GameObject gameObjectForgotUsername;
 
     private NetworkManager networkManager;
     private SceneHandler sceneHandler;
@@ -29,5 +32,28 @@ public class UIForgotData : MonoBehaviour
         buttonBack.interactable = false;
 
         networkManager.ForgotPassword(inputFieldUsernameOrEMail.text);
+    }
+    public void OnButtonSendUsername()
+    {
+        buttonSendUsername.interactable = false;
+        buttonBack.interactable = false;
+
+        networkManager.ForgotPassword(inputFieldUsernameOrEMail.text);
+    }
+
+    public void OnButtonTabPassword()
+    {
+        if(!gameObjectForgotPassword.activeSelf)
+            gameObjectForgotPassword.SetActive(true);
+        if (gameObjectForgotUsername.activeSelf)
+            gameObjectForgotUsername.SetActive(false);
+    }
+
+    public void OnButtonTapUsername()
+    {
+        if (!gameObjectForgotUsername.activeSelf)
+            gameObjectForgotUsername.SetActive(true);
+        if (gameObjectForgotPassword.activeSelf)
+            gameObjectForgotPassword.SetActive(false);
     }
 }
