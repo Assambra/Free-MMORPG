2c046d17dd28b4549f8abef6793acbefc96c9746
Philipp Leutner <philipp.leutner@assambra.com>
2023-08-04 21:08:59 +0200

    Cleanup Account UserLoginController Free-Account-Server, add some guest password check;

--- Free-Account-Server/free-account-server-plugin/src/main/java/com/assambra/plugin/controller/UserLoginController.java
+++ Free-Account-Server/free-account-server-plugin/src/main/java/com/assambra/plugin/controller/UserLoginController.java
@@ -1,13 +1,7 @@
 package com.assambra.plugin.controller;
 
-import com.assambra.common.entity.Account;
-import com.assambra.common.service.AccountService;
-import com.assambra.plugin.service.WelcomeService;
-
-import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
 import com.tvd12.ezyfox.core.annotation.EzyEventHandler;
-import com.tvd12.ezyfox.security.EzySHA256;
 import com.tvd12.ezyfoxserver.constant.EzyLoginError;
 import com.tvd12.ezyfoxserver.context.EzyPluginContext;
 import com.tvd12.ezyfoxserver.controller.EzyAbstractPluginEventController;
@@ -18,39 +12,14 @@ import static com.tvd12.ezyfoxserver.constant.EzyEventNames.USER_LOGIN;
 
 @EzySingleton
 @EzyEventHandler(USER_LOGIN)
-public class UserLoginController extends EzyAbstractPluginEventController<EzyUserLoginEvent> {
-
-    @EzyAutoBind
-    private WelcomeService welcomeService;
-
-    @EzyAutoBind
-    private AccountService accountService;
-
-
+public class UserLoginController extends EzyAbstractPluginEventController<EzyUserLoginEvent>
+{
     @Override
-    public void handle(EzyPluginContext ctx, EzyUserLoginEvent event) {
-        logger.info("{} login in", welcomeService.welcome(event.getUsername()));
-
-        String username = event.getUsername();
-        String password = encodePassword(event.getPassword());
-
-        Account account = accountService.getAccount(username);
-
-        if(event.getUsername().contains("Guest#"))
+    public void handle(EzyPluginContext ctx, EzyUserLoginEvent event)
+    {
+        if(event.getUsername().contains("Guest#") && event.getPassword().contains("Assambra"))
             logger.info("Guest logged in: {}", event.getUsername());
         else
-        {
-            if(!account.getPassword().equals(password))
-            {
-                throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
-            }
-
-            logger.info("user and password match, accept user {}", username);
-        }
-    }
-
-    private String encodePassword(String password)
-    {
-        return EzySHA256.cryptUtfToLowercase(password);
+            throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
     }
 }
