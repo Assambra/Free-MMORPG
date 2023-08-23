0af63f82f63b8140c0ffbc112ccb30d5f8c644e1
Philipp Leutner <philipp.leutner@assambra.com>
2023-08-23 17:45:56 +0200

    Added two idle animations with less motion its better to modify the character, new AnimationControllers for Humanoid Male and Humanoid Female. Added this animation controllers to the uma Race Animation Controller list.

--- Free-Client/Assets/Free-Client/Scripts/Player.cs
+++ Free-Client/Assets/Free-Client/Scripts/Player.cs
@@ -6,4 +6,26 @@ public class Player : MonoBehaviour
 {
     [field: SerializeField] public DynamicCharacterAvatar Avatar { get; private set; }
 
+    private CapsuleCollider capsuleCollider;
+
+    private void Update()
+    {
+        GetCenterOfThePlayer();
+    }
+
+    public Vector3 GetCenterOfThePlayer()
+    {
+        capsuleCollider = Avatar.gameObject.GetComponent<CapsuleCollider>();
+        
+        if (capsuleCollider != null)
+        {
+            Debug.Log(capsuleCollider.center);
+            return capsuleCollider.center;
+        }
+        else
+        {
+            Debug.Log("No Capsule Colider found!");
+            return Vector3.zero;
+        }
+    }
 }
