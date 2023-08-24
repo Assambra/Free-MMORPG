4b0c89bf69f71e69155614d878a0acc135f7c572
Philipp Leutner <philipp.leutner@assambra.com>
2023-08-24 22:18:31 +0200

    Created a Scriptable Object CharacterTemplate to save default Character strings, Created a new NoneGameScene CharacterTemplateBuilder there we can create and save or load template files and all needed scripts Created two new CharacterTemplates DefaultHumanMale and DefaultHumanFemale;

---
+++ Free-Client/Assets/Free-Client/Scenes/NoneGameScenes/CharacterTemplateBuilder/Scripts/CharacterTemplateBuilder.cs
@@ -0,0 +1,71 @@
+using UMA;
+using UMA.CharacterSystem;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CharacterTemplateBuilder : MonoBehaviour
+{
+    [SerializeField] private DynamicCharacterAvatar avatar;
+
+    [Tooltip("Insert the CharacterTemplate file to write the uma character string")]
+    [SerializeField] CharacterTemplate characterTemplate;
+
+    [SerializeField] private string umaCharacterString;
+
+    [SerializeField] private Button buttonSave;
+    [SerializeField] private Button buttonLoad;
+
+    private UMAData umaData;
+
+    private bool doOnce = false;
+
+    private void Awake()
+    {
+        //avatar = avatar.gameObject.GetComponent<DynamicCharacterAvatar>();
+    }
+
+    private void Start()
+    {
+        umaData = avatar.umaData;
+    }
+
+    private void Update()
+    {
+        if(!doOnce)
+        {
+            doOnce = true;
+            umaData.CharacterUpdated.AddListener(new UnityAction<UMAData>(OnCharacterUpdated));
+        }
+    }
+
+
+    public void OnButtonSave()
+    {
+        characterTemplate.UmaCharacterString = UMAHelper.GetAvatarString(avatar);
+        umaCharacterString = characterTemplate.UmaCharacterString;
+    }
+
+    public void OnButtonLoad()
+    {
+        buttonSave.interactable = false;
+        buttonLoad.interactable = false;
+
+        UMAHelper.SetAvatarString(avatar, characterTemplate.UmaCharacterString);
+    }
+
+    public void OnButtonClear()
+    {
+        characterTemplate.UmaCharacterString = "";
+    }
+
+    private void OnCharacterUpdated(UMAData data)
+    {
+        buttonSave.interactable = true;
+        buttonLoad.interactable = true;
+
+        Debug.Log("OnCharacterUpdated");
+        umaCharacterString = UMAHelper.GetAvatarString(avatar);
+        umaData.CharacterUpdated.RemoveListener(new UnityAction<UMAData>(OnCharacterUpdated));
+    }
+}
