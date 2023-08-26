fa7ef9e9421c308a9128e65876e17b9e3c02c577
Philipp Leutner <philipp.leutner@assambra.com>
2023-08-26 14:40:30 +0200

    Implementing the UICameraMover on the Scene: CreateCharacter, not implementet atm the toggle auto focus;

--- Free-Client/Assets/Free-Client/Scripts/UserInterface/PreGame/UICameraMover.cs
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/PreGame/UICameraMover.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UICameraMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button buttonCameraUp;
+    [SerializeField] private Button buttonCameraDown;
+
+    [SerializeField] private float cameraMoveSpeed = 0.25f;
+    [SerializeField] private float cameraMinOffset = 0.27f;
+    [SerializeField] private float cameraMaxOffset = 2.75f;
+
+    private ButtonPressedChecker checkerButtonCameraUp;
+    private ButtonPressedChecker checkerButtonCameraDown;
+
+    private void Awake()
     {
-        
+        checkerButtonCameraUp = buttonCameraUp.gameObject.GetComponent<ButtonPressedChecker>();
+        checkerButtonCameraDown = buttonCameraDown.gameObject.GetComponent<ButtonPressedChecker>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (checkerButtonCameraUp.IsButtonPressed)
+            OnButtonCameraUpPressed();
+        if (checkerButtonCameraDown.IsButtonPressed)
+            OnButtonCameraDownPressed();
+    }
+
+    private void OnButtonCameraUpPressed()
+    {
+        Vector3 currentCameraOffset = GameManager.Instance.cameraController.GetCameraOffset();
+        float newOffset = ClampOffset(currentCameraOffset.y + (cameraMoveSpeed * Time.deltaTime));
+        GameManager.Instance.cameraController.SetCameraOffset(new Vector3(currentCameraOffset.x, newOffset , currentCameraOffset.z));
+    }
+
+    private void OnButtonCameraDownPressed()
+    {
+        Vector3 currentCameraOffset = GameManager.Instance.cameraController.GetCameraOffset();
+        float newOffset = ClampOffset(currentCameraOffset.y + (-cameraMoveSpeed * Time.deltaTime));
+        GameManager.Instance.cameraController.SetCameraOffset(new Vector3(currentCameraOffset.x, newOffset , currentCameraOffset.z));
+    }
+
+    private float ClampOffset(float value)
+    {
+        return Mathf.Clamp(value, cameraMinOffset, cameraMaxOffset);
     }
 }
