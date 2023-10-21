538d713aae8be187f025b756834b22458b563c29
Dung Ta Van <itprono3@gmail.com>
2023-10-21 21:59:02 +0700

    update PlayerService and ClientTest

--- Free-Game-Server/free-game-server-app-api/src/main/java/com/assambra/game/app/constant/Errors.java
+++ Free-Game-Server/free-game-server-app-api/src/main/java/com/assambra/game/app/constant/Errors.java
@@ -3,6 +3,7 @@ package com.assambra.game.app.constant;
 public final class Errors {
 
     public static final int UNKNOWN = 1;
+    public static final int NOT_A_PLAYER = 2;
 
     private Errors() {}
 }
--- Free-Game-Server/free-game-server-app-api/src/main/java/com/assambra/game/app/service/PlayerService.java
+++ Free-Game-Server/free-game-server-app-api/src/main/java/com/assambra/game/app/service/PlayerService.java
@@ -2,25 +2,32 @@ package com.assambra.game.app.service;
 
 import com.assambra.game.app.model.PlayerInputModel;
 import com.assambra.game.app.utils.PlayerMovementUtils;
-import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
+import com.tvd12.ezyfox.core.exception.EzyBadRequestException;
 import com.tvd12.ezyfox.util.EzyLoggable;
 import com.tvd12.gamebox.entity.MMOPlayer;
 import com.tvd12.gamebox.math.Vec3;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 
-@Setter
+import static com.assambra.game.app.constant.Errors.NOT_A_PLAYER;
+
 @EzySingleton
+@AllArgsConstructor
 public class PlayerService extends EzyLoggable {
 
-    @EzyAutoBind
-    RoomService roomService;
-    @EzyAutoBind
-    CharacterService characterService;
+    private final RoomService roomService;
 
-    public void handlePlayerInput(String playerName, PlayerInputModel model)
-    {
+    private final CharacterService characterService;
+
+    public void handlePlayerInput(String playerName, PlayerInputModel model) {
         MMOPlayer player = roomService.getPlayer(playerName);
+        // if user hasn't joined any game, reject the request
+        if (player == null) {
+            throw new EzyBadRequestException(
+                NOT_A_PLAYER,
+                "your_are_not_a_player"
+            );
+        }
         synchronized (player){
             Vec3 currentRotation = player.getRotation();
             Vec3 nextRotation = PlayerMovementUtils.getNextRotation(currentRotation, model);
