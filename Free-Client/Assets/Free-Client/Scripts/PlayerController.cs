a5ad951c2098c30cff2d24470f56001c0c864bca
Philipp Leutner <philipp.leutner@assambra.com>
2023-11-05 00:05:42 +0100

    UserInputs now send via udp, full cleanup playerControllerscript

--- Free-Client/Assets/Free-Client/Scripts/PlayerController.cs
+++ Free-Client/Assets/Free-Client/Scripts/PlayerController.cs
@@ -7,12 +7,7 @@ public class PlayerController : MonoBehaviour
     private bool[] inputs;
     private int clientTick;
 
-    private Vector3 currentPosition;
     private Vector3 lastPosition;
-    private Vector3 nextPosition;
-    private Quaternion currentRotation;
-    private Quaternion nextRotation;
-
     private int animcounter;
 
     private void Start()
@@ -22,77 +17,28 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if(Player.IsLocalPlayer)
-        {
-            if (currentRotation != nextRotation)
-            {
-                var step = Time.fixedDeltaTime * 100f;
-                Player.transform.rotation = Quaternion.RotateTowards(Player.transform.rotation, nextRotation, step);
-                currentRotation = Player.transform.rotation;
-            }
-
-            if (currentPosition != nextPosition)
-            {
-                var step = Time.fixedDeltaTime * 5.424f;
-                Player.transform.position = Vector3.MoveTowards(Player.transform.position, nextPosition, step);
-                currentPosition = Player.transform.position;
-            }
-        }
-        else
-        {
-            Player.transform.rotation = nextRotation;
-            /*
-            if (currentRotation != nextRotation)
-            {
-                var step = Time.fixedDeltaTime * 100f;
-                player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, nextRotation, step);
-                currentRotation = player.transform.rotation;
-            }
-            */
-            if (currentPosition != nextPosition)
-            {
-                var step = Time.fixedDeltaTime * 5.424f;
-                Player.transform.position = Vector3.MoveTowards(Player.transform.position, nextPosition, step);
-                currentPosition = Player.transform.position;
-            }
-            
-            //player.transform.position = nextPosition;
-        }
-
-        
-        
-        /*
-        if (currentRotation != nextRotation)
-        {
-            var step = Time.fixedDeltaTime * 150f;
-            player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, nextRotation, step);
-        }
-        */
-
-        if (!Player.IsLocalPlayer)
-            return;
-
-        GetUserInput();
-        clientTick++;
-
-        if (inputs[0] || inputs[1] || inputs[2] || inputs[3])
-        {
-            SendInput();
-        }
-
-        for (int i = 0; i < inputs.Length; i++)
-            inputs[i] = false;
-    }
-
-    private void FixedUpdate()
-    {
-        if(Player.Animator != null)
+        if (Player.Animator != null)
             CalculateAnimations();
-        
+
         lastPosition = transform.position;
 
         if (!Player.IsLocalPlayer)
             return;
+    }
+
+    private void FixedUpdate()
+    {
+        clientTick++;
+
+        GetUserInput();
+
+        if (inputs[0] || inputs[1] || inputs[2] || inputs[3])
+        {
+            SendInput();
+
+            for (int i = 0; i < inputs.Length; i++)
+                inputs[i] = false;
+        }
     }
 
     private void GetUserInput()
@@ -127,7 +73,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             animcounter++;
-            if (animcounter > 3)
+            if (animcounter > 5)
             {
                 Player.Animator.SetFloat("Vertical", 0f);
                 animcounter = 0;
@@ -142,13 +88,11 @@ public class PlayerController : MonoBehaviour
 
     public void Move(Vector3 nextPosition)
     {
-        this.nextPosition = nextPosition;
-        //player.transform.position = nextPosition;
+        Player.transform.position = nextPosition;
     }
 
     public void Rotate(Vector3 rotation)
     {
-        nextRotation = Quaternion.Euler(rotation);
-        //player.transform.rotation = Quaternion.Euler(rotation);
+        Player.transform.rotation = Quaternion.Euler(rotation);
     }
 }
