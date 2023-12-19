ca8f7092ca1a33d460588348ef47fa2d84abc740
Philipp Leutner <philipp.leutner@assambra.com>
2023-12-19 12:30:40 +0100

    implemented popup system removed all code that use old client log

--- Free-Client/Assets/Free-Client/ScriptableObjects/ClientLogFile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ClientLogFile", menuName = "Assambra/ClientLogFile", order = 1)]
-public class ClientLogFile : ScriptableObject
-{
-    public string File;
-}
