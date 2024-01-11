ac9e3e2b6a101510f3aa6f2c953d5f0aa223cfbc
Philipp Leutner <philipp.leutner@assambra.com>
2024-01-11 10:31:08 +0100

    Account activation mailbody and html template

--- Free-Account-Server/free-account-server-common/src/main/java/com/assambra/account/common/mail/mailbodys/AccountActivationCodeMailBody.java
+++ Free-Account-Server/free-account-server-common/src/main/java/com/assambra/account/common/mail/mailbodys/AccountActivationCodeMailBody.java
@@ -1,2 +1,39 @@
-package com.assambra.account.common.mail.mailbodys;public class AccountActivationCodeMailBody {
+package com.assambra.account.common.mail.mailbodys;
+
+import com.assambra.account.common.mail.interfaces.MailBody;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+public class AccountActivationCodeMailBody implements MailBody {
+
+    private Configuration configuration;
+    private Template bodyTemplate;
+    private Map<String, Object> dataModel;
+
+    @Override
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void loadTemplate() throws IOException {
+        bodyTemplate = configuration.getTemplate("account_activation_code_body.ftl");
+    }
+
+    @Override
+    public String buildBody() throws IOException, TemplateException {
+        StringWriter bodyWriter = new StringWriter();
+        bodyTemplate.process(dataModel, bodyWriter);
+        return bodyWriter.toString();
+    }
+
+    @Override
+    public void setDataModel(Map<String, Object> dataModel) {
+        this.dataModel = dataModel;
+    }
 }
