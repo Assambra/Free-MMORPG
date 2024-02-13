7a0a7239d3319fbbc4d2cec5b50291c6a3cc7a1c
Philipp Leutner <philipp.leutner@assambra.com>
2024-02-13 21:01:52 +0100

    Updated GameVariables

--- Free-Client/Assets/Free-Client/Scripts/Constants/GameConstants.cs
+++ Free-Client/Assets/Free-Client/Scripts/Constants/GameConstants.cs
@@ -3,8 +3,8 @@ public sealed class GameConstants
     // Input Validator
     public const int MIN_USERNAME_LENGTH = 2;
     public const int MAX_USERNAME_LENGTH = 20;
-    public static readonly string[] DISALLOWED_NAMES = { "admin", "gm", "game", "master" };
-    public static readonly char[] VALID_USERNAME_CHARACTERS = { '_', '-', '.' };
+    public static readonly string[] DISALLOWED_NAMES = { "admin", "administrator", "game master", "gamemaster", "gm" };
+    public static readonly char[] VALID_USERNAME_CHARACTERS = { '_', '-', '.', ' ' };
 
     private GameConstants() { }
 }
