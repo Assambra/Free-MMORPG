9f97bcc7cb9511944566c4af97f98fdedfd24094
Assambra <philipp.leutner@assambra.com>
2024-11-30 23:44:33 +0100

    New enum EntityType to differantiate entity types

---
+++ Free-Client/Assets/Free-Client/Scripts/Enums/EntityType.cs
@@ -0,0 +1,12 @@
+namespace Assambra.FreeClient
+{
+    public enum EntityType
+    {
+        None = 0,
+        Player = 1,
+        NPC = 2,
+        Buildings = 3,
+        Effects = 4,
+    }
+}
+
