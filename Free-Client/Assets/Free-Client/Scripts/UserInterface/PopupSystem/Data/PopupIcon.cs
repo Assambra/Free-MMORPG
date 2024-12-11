c4c204354e4e3b683859b107a6e7e8cf676ae583
Assambra <philipp.leutner@assambra.com>
2024-12-11 18:48:46 +0100

    New PopupIcon class

---
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/PopupSystem/Data/PopupIcon.cs
@@ -0,0 +1,13 @@
+using Assambra.FreeClient.UserInterface.PopupSystem.Enum;
+using System;
+using UnityEngine;
+
+namespace Assambra.FreeClient.UserInterface.PopupSystem.Data
+{
+    [Serializable]
+    public class PopupIcon
+    {
+        public PopupType Type;
+        public Sprite Icon;
+    }
+}
