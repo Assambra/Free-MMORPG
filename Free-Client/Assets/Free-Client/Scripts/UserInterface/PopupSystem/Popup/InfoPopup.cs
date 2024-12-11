a9639018d7c4baae871c15f24db2e0eac7ff1d1f
Assambra <philipp.leutner@assambra.com>
2024-12-11 18:37:55 +0100

    Renamed InformationPopup to InfoPopup

--- Free-Client/Assets/Free-Client/Scripts/UserInterface/PopupSystem/Popup/InfoPopup.cs
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/PopupSystem/Popup/InfoPopup.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 namespace Assambra.FreeClient.UserInterface.PopupSystem.Popup
 {
-    public class InformationPopup : BasePopup
+    public class InfoPopup : BasePopup
     {
         [SerializeField] Button _oKButton;
 
