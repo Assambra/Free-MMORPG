ee8f40aeda5eb291f564334dd98ce65e9ad55c01
Assambra <philipp.leutner@assambra.com>
2024-12-12 17:44:58 +0100

    Login empty fields dont use a error now it is a info popup

--- Free-Client/Assets/Free-Client/Scripts/UserInterface/PreGame/UILogin.cs
+++ Free-Client/Assets/Free-Client/Scripts/UserInterface/PreGame/UILogin.cs
@@ -42,7 +42,7 @@ namespace Assambra.FreeClient.UserInterface
                 NetworkManager.Instance.Login(_username, _password);
             }
             else
-                ErrorPopup("Username or password cannot be empty. Please enter both and try again.");
+                InformationPopup("Username or password cannot be empty. Please enter both and try again.");
         }
 
         public void OnButtonNeedAccount()
@@ -55,11 +55,11 @@ namespace Assambra.FreeClient.UserInterface
             GameManager.Instance.ChangeScene(Scenes.ForgotData);
         }
 
-        private void ErrorPopup(string error)
+        private void InformationPopup(string info)
         {
-            string title = "Error";
+            string title = "Info";
 
-            PopupManager.Instance.ShowInfoPopup<InfoPopup>(PopupType.Error, title, error);
+            PopupManager.Instance.ShowInfoPopup<InfoPopup>(PopupType.Info, title, info);
         }
     }
 }
